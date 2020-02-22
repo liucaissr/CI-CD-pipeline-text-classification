c3c09c70dceae66380c91034eb887f979635d73a
liucaissr <liucaissr@gmail.com>
2020-02-22 20:35:48 +0000

    change dockerfile

--- train/runDocker.sh
+++ train/runDocker.sh
@@ -4,35 +4,35 @@ echo "hallo shell"
 repopath=`git rev-parse --show-toplevel`
 
 init(){
-    docker rmi -f qc-contactrequest-image:$1
-    docker rm qc-contactrequest-container-$1
+    docker rmi -f qc-image:$1
+    docker rm qc-container-$1
 }
 
 Dep() {
   echo "hallo igor";
-  docker build -f train/src/main/docker/Dockerfile -t qc-contactrequest-image:$1 .
+  docker build -f train/src/main/docker/Dockerfile -t qc-image:$1 .
   # run nbvonvert
-  docker create --name qc-contactrequest-container-$1 -v ${repopath}:/app -i -t -p 8080:8080 qc-contactrequest-image:$1  \
+  docker create --name qc-container-$1 -v ${repopath}:/app -i -t -p 8080:8080 qc-image:$1  \
   jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=-1 qc-contact-request-deletionreason.ipynb \
   jupyter nbconvert qc-contact-request-deletionreason.ipynb --output-dir='output' --output='commit1.nbconvert.html'
-  docker rmi -f qc-contactrequest-image:$1
-  docker rm qc-contactrequest-container-$1
+  docker rmi -f qc-image:$1
+  docker rm qc-container-$1
 }
 
 Dev() {
   echo "hallo developer";
-  imageexisted=`docker images --format "{{.Repository}}:{{.Tag}}" | grep qc-contactrequest-image:$1`
+  imageexisted=`docker images --format "{{.Repository}}:{{.Tag}}" | grep qc-image:$1`
   if [[ -z "$imageexisted" || "$2" == 'initImage' ]] ;then
-    docker rmi -f qc-contactrequest-image:$1
-    docker rm qc-contactrequest-container-$1
-    docker build -f ${repopath}/train/src/main/docker/Dockerfile -t qc-contactrequest-image:$1 .
-    docker create --name qc-contactrequest-container-$1 -v ~/.gitconfig:/etc/gitconfig -v ${repopath}:/app -i -t -p 8080:8080 qc-contactrequest-image:$1 bash
+    docker rmi -f qc-image:$1
+    docker rm qc-container-$1
+    docker build -f ${repopath}/train/src/main/docker/Dockerfile -t qc-image:$1 .
+    docker create --name qc-container-$1 -v ~/.gitconfig:/etc/gitconfig -v ${repopath}:/app -i -t -p 8080:8080 qc-image:$1 bash
   fi
   if [[ "$2" == 'initContainer' ]] ;then
-    docker rm qc-contactrequest-container-$1
-    docker create --name qc-contactrequest-container-$1 -v ~/.gitconfig:/etc/gitconfig -v ${repopath}:/app -i -t -p 8080:8080 qc-contactrequest-image:$1 bash
+    docker rm qc-container-$1
+    docker create --name qc-container-$1 -v ~/.gitconfig:/etc/gitconfig -v ${repopath}:/app -i -t -p 8080:8080 qc-image:$1 bash
   fi
-  docker start -a -i qc-contactrequest-container-$1
+  docker start -a -i qc-container-$1
 }
 
 "$@"
